2df732e606e397cdac025fd5cff3e71be33299b2
David Irvine <david.irvine@maidsafe.net>
2026-05-20 15:06:25 +0100

    clawpatch: Listener subprocess is only cleaned up on the happy path
    finding: fnd_sig-feat-test-suite-daf8cd9e9e-6_2ee8df7755
    category: build-release
    severity: low

--- tests/cli_connect_smoke.rs
+++ tests/cli_connect_smoke.rs
@@ -1,9 +1,10 @@
 #![allow(clippy::unwrap_used, clippy::expect_used)]
 
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
-use std::process::{Command, Stdio};
+use std::process::{Child, Command, Stdio};
 use std::sync::mpsc;
+use std::thread::JoinHandle;
 use std::time::Duration;
 
 fn free_localhost_addr() -> SocketAddr {
@@ -31,50 +32,118 @@ fn parse_local_identity_peer_id(line: &str) -> Option<String> {
     }
 }
 
+struct ListenerProcess {
+    child: Option<Child>,
+    stdout_handle: Option<JoinHandle<String>>,
+    stderr_handle: Option<JoinHandle<String>>,
+}
+
+impl ListenerProcess {
+    fn new(mut child: Child, peer_id_tx: Option<mpsc::Sender<String>>) -> Self {
+        let listener_stdout = child
+            .stdout
+            .take()
+            .expect("listener stdout should be piped");
+        let listener_stderr = child
+            .stderr
+            .take()
+            .expect("listener stderr should be piped");
+
+        Self {
+            child: Some(child),
+            stdout_handle: Some(spawn_output_drain(listener_stdout, peer_id_tx.clone())),
+            stderr_handle: Some(spawn_output_drain(listener_stderr, peer_id_tx)),
+        }
+    }
+
+    fn is_running(&mut self) -> bool {
+        self.child
+            .as_mut()
+            .expect("listener child should be present")
+            .try_wait()
+            .expect("check listener liveness")
+            .is_none()
+    }
+
+    fn stop(&mut self) -> (String, String) {
+        self.kill_and_wait();
+        let stdout = self
+            .stdout_handle
+            .take()
+            .expect("listener stdout drain thread should be present")
+            .join()
+            .expect("join listener stdout drain thread");
+        let stderr = self
+            .stderr_handle
+            .take()
+            .expect("listener stderr drain thread should be present")
+            .join()
+            .expect("join listener stderr drain thread");
+        (stdout, stderr)
+    }
+
+    fn kill_and_wait(&mut self) {
+        if let Some(mut child) = self.child.take() {
+            let _ = child.kill();
+            let _ = child.wait();
+        }
+    }
+}
+
+impl Drop for ListenerProcess {
+    fn drop(&mut self) {
+        self.kill_and_wait();
+        if let Some(handle) = self.stdout_handle.take() {
+            let _ = handle.join();
+        }
+        if let Some(handle) = self.stderr_handle.take() {
+            let _ = handle.join();
+        }
+    }
+}
+
+fn spawn_output_drain<R: Read + Send + 'static>(
+    stream: R,
+    peer_id_tx: Option<mpsc::Sender<String>>,
+) -> JoinHandle<String> {
+    std::thread::spawn(move || {
+        let reader = BufReader::new(stream);
+        let mut output = String::new();
+        for line in reader.lines() {
+            let line = line.expect("read listener output line");
+            output.push_str(&line);
+            output.push('\n');
+            if let (Some(tx), Some(peer_id)) = (&peer_id_tx, parse_local_identity_peer_id(&line)) {
+                let _ = tx.send(peer_id);
+            }
+        }
+        output
+    })
+}
+
 #[test]
 fn cli_connect_smoke() {
     let bin = env!("CARGO_BIN_EXE_ant-quic");
     let listen_addr = free_localhost_addr();
 
-    let mut listener = Command::new(bin)
-        .arg("--listen")
-        .arg(listen_addr.to_string())
-        .arg("--no-default-bootstrap")
-        .arg("--json")
-        .arg("--duration")
-        .arg("12")
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .expect("spawn listener ant-quic");
-
-    let listener_stdout = listener
-        .stdout
-        .take()
-        .expect("listener stdout should be piped");
-    let listener_stderr = listener
-        .stderr
-        .take()
-        .expect("listener stderr should be piped");
-    let stdout_handle = std::thread::spawn(move || {
-        let reader = BufReader::new(listener_stdout);
-        for line in reader.lines() {
-            let _ = line.expect("read listener stdout line");
-        }
-    });
-    let stderr_handle = std::thread::spawn(move || {
-        let reader = BufReader::new(listener_stderr);
-        for line in reader.lines() {
-            let _ = line.expect("read listener stderr line");
-        }
-    });
+    let mut listener = ListenerProcess::new(
+        Command::new(bin)
+            .arg("--listen")
+            .arg(listen_addr.to_string())
+            .arg("--no-default-bootstrap")
+            .arg("--json")
+            .arg("--duration")
+            .arg("12")
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .expect("spawn listener ant-quic"),
+        None,
+    );
 
     std::thread::sleep(Duration::from_millis(800));
     assert!(
-        listener
-            .try_wait()
-            .expect("check listener liveness")
-            .is_none(),
+        listener.is_running(),
         "listener should still be running before launching connector"
     );
 
@@ -89,6 +158,8 @@ fn cli_connect_smoke() {
         .stderr(Stdio::piped())
         .output()
         .expect("run connector ant-quic");
+
+    let (_listener_stdout_output, _listener_stderr_output) = listener.stop();
 
     assert!(
         connector.status.success(),
@@ -105,73 +176,29 @@ fn cli_connect_smoke() {
         connector_stdout,
         String::from_utf8_lossy(&connector.stderr)
     );
-
-    let _ = listener.kill();
-    let _ = listener.wait();
-    stdout_handle
-        .join()
-        .expect("join listener stdout drain thread");
-    stderr_handle
-        .join()
-        .expect("join listener stderr drain thread");
 }
 
 #[test]
 fn cli_connect_peer_id_smoke() {
     let bin = env!("CARGO_BIN_EXE_ant-quic");
     let listen_addr = free_localhost_addr();
-
-    let mut listener = Command::new(bin)
-        .arg("--listen")
-        .arg(listen_addr.to_string())
-        .arg("--no-default-bootstrap")
-        .arg("--json")
-        .arg("--full-key")
-        .arg("--duration")
-        .arg("12")
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .expect("spawn listener ant-quic");
-
-    let listener_stdout = listener
-        .stdout
-        .take()
-        .expect("listener stdout should be piped");
-    let listener_stderr = listener
-        .stderr
-        .take()
-        .expect("listener stderr should be piped");
     let (tx, rx) = mpsc::channel();
 
-    let stdout_tx = tx.clone();
-    let stdout_handle = std::thread::spawn(move || {
-        let reader = BufReader::new(listener_stdout);
-        let mut all_stdout = String::new();
-        for line in reader.lines() {
-            let line = line.expect("read listener stdout line");
-            all_stdout.push_str(&line);
-            all_stdout.push('\n');
-            if let Some(peer_id) = parse_local_identity_peer_id(&line) {
-                let _ = stdout_tx.send(peer_id);
-            }
-        }
-        all_stdout
-    });
-
-    let stderr_handle = std::thread::spawn(move || {
-        let reader = BufReader::new(listener_stderr);
-        let mut all_stderr = String::new();
-        for line in reader.lines() {
-            let line = line.expect("read listener stderr line");
-            all_stderr.push_str(&line);
-            all_stderr.push('\n');
-            if let Some(peer_id) = parse_local_identity_peer_id(&line) {
-                let _ = tx.send(peer_id);
-            }
-        }
-        all_stderr
-    });
+    let mut listener = ListenerProcess::new(
+        Command::new(bin)
+            .arg("--listen")
+            .arg(listen_addr.to_string())
+            .arg("--no-default-bootstrap")
+            .arg("--json")
+            .arg("--full-key")
+            .arg("--duration")
+            .arg("12")
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .expect("spawn listener ant-quic"),
+        Some(tx),
+    );
 
     let peer_id = rx
         .recv_timeout(Duration::from_secs(5))
@@ -179,10 +206,7 @@ fn cli_connect_peer_id_smoke() {
 
     std::thread::sleep(Duration::from_millis(300));
     assert!(
-        listener
-            .try_wait()
-            .expect("check listener liveness")
-            .is_none(),
+        listener.is_running(),
         "listener should still be running before launching connector"
     );
 
@@ -199,6 +223,16 @@ fn cli_connect_peer_id_smoke() {
         .output()
         .expect("run peer-id connector ant-quic");
 
+    let (listener_stdout_output, listener_stderr_output) = listener.stop();
+
+    assert!(
+        listener_stdout_output.contains("\"event\":\"local_identity\"")
+            || listener_stderr_output.contains("\"event\":\"local_identity\""),
+        "listener output should contain local_identity JSON event; stdout={} stderr={}",
+        listener_stdout_output,
+        listener_stderr_output
+    );
+
     assert!(
         connector.status.success(),
         "peer-id connector should exit successfully; status={:?}, stderr={} stdout={}",
@@ -213,17 +247,5 @@ fn cli_connect_peer_id_smoke() {
         "peer-id connector stdout should contain peer_connected JSON event; stdout={} stderr={}",
         connector_stdout,
         String::from_utf8_lossy(&connector.stderr)
-    );
-
-    let _ = listener.kill();
-    let _ = listener.wait();
-    let listener_stdout_output = stdout_handle.join().expect("join stdout reader thread");
-    let listener_stderr_output = stderr_handle.join().expect("join stderr reader thread");
-    assert!(
-        listener_stdout_output.contains("\"event\":\"local_identity\"")
-            || listener_stderr_output.contains("\"event\":\"local_identity\""),
-        "listener output should contain local_identity JSON event; stdout={} stderr={}",
-        listener_stdout_output,
-        listener_stderr_output
     );
 }
